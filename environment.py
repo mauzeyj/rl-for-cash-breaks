17b33bfea34c482b000d460e63befeeb480856d5
Jason Mauzey <jmauzey@clearwateranalytics.com>
2019-04-09 13:53:05 -0600

    dollar_values and number of rows working

--- environment.py
+++ environment.py
@@ -5,35 +5,30 @@ from gym.utils import seeding
 
 class cash_breaks():
     def __init__(self):
-        self.action_space = spaces.Discrete(2)
-        self.observation_space = spaces.Box(np.array([0, -10]), np.array([31, 31]), dtype=np.float32)
+        self.action_space = spaces.Discrete(4)
+        self.observation_space = spaces.Box(low=-10000, high=10000, shape=(100,3), dtype=np.float32)
         self.state = None
+        self.target_value = None
 
     def seed(self, seed=None):
         self.np_random, seed = seeding.np_random(seed)
         return [seed]
 
-    def reset(self):
+    def reset(self): # get dollar values
+        start = np.zeros(100)
+        values = cash_breaks.dollar_values()
+        start[0:values.shape[0],] = values
         self.state = np.array(
             [twenty_one.card(self), twenty_one.card(self)])  # dealer player reward done info
         return self.state
 
-    def card(self):
-        return np.random.randint(1, 11)
+    def dollar_values(self):
+        return np.random.random_integers(low=-6000, high=6000, size=(cash_breaks.number_of_rows(), 1))
 
-    def color(self):
-        n = np.random.rand()
-        if n <= .333333:
-            return 'r'
-        else:
-            return 'b'
+    def number_of_rows(self):
+        return np.random.randint(10,80)
 
-    def draw(self):
-        if twenty_one.color(self) == 'r':
-            c = twenty_one.card(self) * -1
-        else:
-            c = twenty_one.card(self)
-        return c
+    def sum_value(self):
 
     def step(self, action):
         assert self.action_space.contains(action), "%r (%s) invalid" % (action, type(action))
